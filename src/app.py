6d8c1a0221b41dd15e0c4269bce4c5bd5b238550
James Pyne <james.pyne.13@ucl.ac.uk>
2018-06-03 14:44:46 +0100

    RNN class created; forward() method tests passing.

--- src/app.py
+++ src/app.py
@@ -1,3 +1,22 @@
+import torch
+import torch.nn as nn
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+
+        self.hidden_size = hidden_size
+
+        self.input_to_hidden = nn.Linear(input_size + hidden_size, hidden_size)
+        self.input_to_output = nn.Linear(input_size + hidden_size, output_size)
+        self.log_softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.input_to_hidden(combined)
+        output = self.input_to_output(combined)
+        output = self.log_softmax(output)
+        return output, hidden
 
 
 def main():
--- test/test_app.py
+++ test/test_app.py
@@ -1,8 +1,23 @@
 import app
+import torch
 
-class TestApp:
+class TestMain:
     def test_main_completes(self):
         """
         This test method checks that the main method runs to completion, returning exit code 0.
         """
         assert app.main() == 0
+
+class TestRNN:
+    def test_RNN_forward(self):
+        """It contains a forward() that takes a <1 x n_input> input tensor and <1 x n_hidden> tensor,
+        concatenates, then processes them to return a <1 x n_output> output and a <1 x n_hidden> tensor for the
+        next step.
+        """
+        input = 5
+        hidden = 6
+        output = 1
+        nn = app.RNN(input, hidden, output)
+        forward_result = nn.forward(torch.rand((1,input)), torch.rand((1,hidden)))
+        assert [1, output] == list(forward_result[0].size())
+        assert [1, hidden] == list(forward_result[1].size())
